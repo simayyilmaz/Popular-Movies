2034d31149e8b7056e0361caf7d54f6dab05fb4d
simay <simay.yilmaz@digiturk.com.tr>
2022-11-22 14:58:27 +0300

    click

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,8 +1,10 @@
 import './style.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import useFetch from '../../hooks/useFecth';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+
+
 
 const Movie = () => {
   const location = useLocation();
@@ -16,20 +18,26 @@ const {data, loading, error, errorDetails} = useFetch(`https://api.themoviedb.or
 
 
   return (
-    <div> 
+    <div className='container'> 
 
        <Header /> 
+       <Link to={"/"}>
+       <button class="btn" ><strong>go back</strong></button>
+       </Link>
+       <div>
       <br/><img className='imagee' src={`https://image.tmdb.org/t/p/w500_and_h282_face/${data?.poster_path}`} alt="movie_details" /><br/>
+      </div>
+
       <div className='details'>
       <p className='title'>{data?.original_title}</p><br/>
-      <p className='datee'>Tarih: {data?.release_date}</p><br/>
-      <p className='time'>Süre:  {data?.runtime} min</p><br/>
-      <p className='overview'>Açıklama:  {data?.overview}</p><br/>
-      <p className='tagline'>Replik:  {data?.tagline}</p><br/>
-      <p className='language'>Dil:   {data?.original_language}</p><br/>
-      <p className='country'>Ülke: production_countries}</p><br/>
-      <p className='vote'>IMDB Reyting:  {data?.vote_average}</p><br/>
-      <p className='votecount'>Oy Sayısı:  {data?.vote_count}</p><br/>
+      <p className='datee'><strong>Tarih:</strong> {data?.release_date}</p><br/>
+      <p className='time'><strong>Süre:</strong>  {data?.runtime} min</p><br/>
+      <p className='overview'><strong>Açıklama: </strong> {data?.overview}</p><br/>
+      <p className='tagline'><strong>Replik: </strong> {data?.tagline}</p><br/>
+      <p className='language'><strong>Dil: </strong>  {data?.original_language}</p><br/>
+      <p className='country'><strong>Ülke:</strong> production_countries}</p><br/>
+      <p className='vote'><strong>IMDB Reyting:</strong>  {data?.vote_average}</p><br/>
+      <p className='votecount'><strong>Oy Saysı: </strong> {data?.vote_count}</p><br/>
       </div>
 
       <Footer />
